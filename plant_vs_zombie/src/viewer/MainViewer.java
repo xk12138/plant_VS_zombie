821c3dad59ef9280af589db3708d746abe459bb5
xk12138 <38340557+xk12138@users.noreply.github.com>
2019-10-12 11:43:34 +0800

    初步让初始界面显示出来了。
    使用JLayeredPane类，初步让初始界面的图片显示了出来，但是还没有增加其他内容以及响应的响应，也还没有对类进行整理。

--- plant_vs_zombie/src/viewer/MainViewer.java
+++ plant_vs_zombie/src/viewer/MainViewer.java
@@ -3,6 +3,7 @@ package viewer;
 import java.awt.*;
 import javax.swing.*;
 
+@SuppressWarnings("serial")
 public class MainViewer extends JFrame {
 	public Container container;
 	
@@ -11,6 +12,17 @@ public class MainViewer extends JFrame {
 		this.setIconImage(new ImageIcon("resource\\images\\interface\\SmallLogo.png").getImage());
 		this.container = this.getContentPane();
 		
+		// 增加窗口的内容
+		JLayeredPane layeredPane = new JLayeredPane();
+		ImageIcon image = new ImageIcon("resource\\images\\interface\\Surface.png");
+		JPanel jp = new JPanel();
+		jp.setBounds(0, 0, image.getIconWidth(), image.getIconHeight());
+		JLabel jl = new JLabel(image);
+		jp.add(jl);
+		layeredPane.add(jp, JLayeredPane.DEFAULT_LAYER);
+		
+		// 让窗口显示出来
+		this.setLayeredPane(layeredPane);
 		this.setSize(905, 640);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
