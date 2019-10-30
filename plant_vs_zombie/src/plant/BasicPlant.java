2d2af66914ca394809ce27af07ecdc263809af17
LiSongyang <email@example.com>
2019-10-30 13:16:59 +0800

    补，刚才好像没提交完
    接上一个

--- plant_vs_zombie/src/plant/BasicPlant.java
+++ plant_vs_zombie/src/plant/BasicPlant.java
@@ -1,10 +1,44 @@
 package plant;
 
+import bullet.BasicBullet;
+
 public class BasicPlant {
 	public int health;      //血量
     public int timer;       //计时器
     public int attackSpeed;    //攻速
+    public boolean attackReady;  //攻击就绪
     public static int coolDown;//CD
     public static int price;   //价格
-
+    
+           //植物所对应的子弹怎么记录？这个地方怎么弄还没想清楚
+           //暂时想的是在继承后的植物类中写一个对应的子弹，没有实际作用，起到记录这个植物用什么子弹的作用
+           //真正的子弹的生成，在行控制器中写
+    public BasicPlant() {
+ 
+    	this.health = 1000;
+    	this.attackSpeed = 0;
+    	coolDown = 0;
+    	price = 25;
+    	this.attackSpeed = 100;
+    	this.timer = this.attackSpeed;
+    	attackReady = (this.timer == this.attackSpeed);
+    }
+    public boolean attackPrepare() { //内部函数————计算攻速计数器和攻击就绪
+    	if(attackReady)
+    		return true;
+    	else {
+    		this.timer++;
+    		attackReady = (this.timer == this.attackSpeed);
+    		return attackReady;
+    	}
+    }
+    public boolean attack(boolean zombieExsit) {//外部函数————给出有无僵尸，得出当前是否攻击，每帧调用一次
+    	attackPrepare();
+    	if(this.attackReady && zombieExsit) {
+    		this.timer = 0;
+    		this.attackReady = (this.timer == this.attackSpeed);
+    		return true;
+    	}else
+    		return false;
+    }
 }
--- plant_vs_zombie/src/plant/PeaShooter.java
+++ plant_vs_zombie/src/plant/PeaShooter.java
@@ -1,5 +1,19 @@
 package plant;
 
-public class PeaShooter {
+import bullet.Pea;
+
+public class PeaShooter extends BasicPlant {
+	
+	//public static Pea bullet(0,0);
+	//暂时记录这种植物对应的子弹，但是有bug，不懂为啥报错
+	
+	public PeaShooter() {
+		this.health = 100;
+    	this.attackSpeed = 100;
+    	this.timer = 0;
+    	this.attackReady = false;
+    	coolDown = 50;
+    	price = 100;
+	}
 
 }
